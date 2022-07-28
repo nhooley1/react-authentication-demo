665b0289d4db3d8591014837e067d1c257811ba0
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2022-07-28 15:48:04 +0100

    basic login register auth set up using firebase

---
+++ src/api.js/api-calls.js
@@ -0,0 +1,37 @@
+import { SIGN_IN, REGISTER } from './api-keys';
+
+const loginOrRegisterApiCall = async (
+  enteredEmail,
+  enteredPassword,
+  isLogin
+) => {
+  let url = '';
+  if (isLogin) {
+    url = SIGN_IN;
+  } else {
+    url = REGISTER;
+  }
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      email: enteredEmail,
+      password: enteredPassword,
+      returnSecureToken: true,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    let errorMessage = 'Authentication failed';
+    console.log(`HTTP error! status: ${response.status}`);
+    throw new Error(errorMessage);
+  }
+
+  const data = await response.json();
+
+  console.log(data);
+};
+
+export { loginOrRegisterApiCall };
---
+++ src/api.js/api-keys.js
@@ -0,0 +1,7 @@
+const SIGN_IN =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAXst_73E44MoLbg0PGXteI25NqvVkm7jM';
+
+const REGISTER =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXst_73E44MoLbg0PGXteI25NqvVkm7jM';
+
+export { SIGN_IN, REGISTER };
