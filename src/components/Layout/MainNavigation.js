fc7545c53ba26ac23862f09ab7a9e9347a5f8e3f
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2022-08-04 16:20:52 +0100

    persist token in local storage, add route guards

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -8,6 +8,10 @@ const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
 
   const { isLoggedIn } = authCtx;
+
+  const onLogoutHandler = () => {
+    authCtx.logout();
+  };
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -27,7 +31,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={onLogoutHandler}>Logout</button>
             </li>
           )}
         </ul>
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,4 +1,5 @@
 import classes from './ProfileForm.module.css';
+import { useHistory } from 'react-router-dom';
 import { useContext } from 'react';
 import { changePasswordApiCall } from '../../api.js/api-calls';
 import AuthContext from '../../store/auth-context';
@@ -6,6 +7,7 @@ import AuthContext from '../../store/auth-context';
 import { useRef } from 'react';
 
 const ProfileForm = () => {
+  const history = useHistory();
   const authCtx = useContext(AuthContext);
   const passwordInputRef = useRef();
 
@@ -13,6 +15,7 @@ const ProfileForm = () => {
     event.preventDefault();
     const enteredPassword = passwordInputRef.current.value;
     changePasswordApiCall(authCtx.token, enteredPassword);
+    history.replace('/');
   };
 
   return (
